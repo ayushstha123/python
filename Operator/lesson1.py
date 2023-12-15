297e3f918a444ca555dbf647fddb6ee6f3b9f2b3
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2023-12-15 15:04:29 +0545

    assignments1 completed

--- Operator/lesson1.py
+++ Operator/lesson1.py
@@ -1,7 +1,10 @@
-meaning=8
+meaning=12
 print('')
 
-if meaning >= 10 :
-    print("YES ITS GREATERRRR!")
-else:
-    print("OH NO ITS SMALLER THAN 10")
+# if meaning >= 10 :
+#     print("YES ITS GREATERRRR!")
+# else:
+#     print("OH NO ITS SMALLER THAN 10")
+
+# ternary operator
+print("greater than 10") if meaning>10 else print("ohh no")
---
+++ basics/assignments/assignment2.py
@@ -0,0 +1,8 @@
+#addition,subtraction,multiplication,division
+a=3
+b=3
+print('Add:', a+b)
+print('sub:',a-b)
+print('multiply:',a*b)
+print('div:',a/b)
+print('exponential:',a**b)
