dbd8a7224d70124e5d206842fa15ba5c710dfcc9
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2023-12-17 11:48:10 +0545

    datatypes

---
+++ Datatypes/datatypes.py
@@ -0,0 +1,50 @@
+#string data type
+
+#literal assignment
+first ="ayush"
+last="shrestha"
+# print(type(first))
+# print(type(first)==str)
+# print(isinstance(first,str))
+
+#construction function
+# pizza=str("pepperoni")
+# print(type(pizza))
+# print(type(pizza)==str)
+# print(isinstance(pizza,str))
+
+#CONCATINATION
+# fullname=first + " "+last
+# print(fullname)
+# fullname+= "!!"
+# print(fullname)
+
+#casting a number to a string
+decade=str(1980)
+print(type(decade))
+
+statement="i like rock music from the "+ decade+"s"
+print(statement)
+
+#multiple lines
+multipleline='''
+
+hey how are you ?
+
+i was just cheeking in bro 
+
+            all good
+
+'''
+print(multipleline)
+
+
+#escaping special characters
+sentence='I\'m back at work !\t hey \n hellowww \\ located ?'
+print(sentence)
+
+#string method
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
