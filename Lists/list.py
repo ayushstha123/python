f8a092ef032076a72dfe80a58bcafdcf20ef881b
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-01-19 22:49:51 +0545

    removing,deleting,clearing,and sorting lists

--- Lists/list.py
+++ Lists/list.py
@@ -6,7 +6,6 @@ emptyList=[]
 print("Dave" in emptyList)# is dave in empty list?
 print(users[0])
 print(users[-1])
-print(users[-1])
 print(users.index('Sam'))#to know the position of the data
 print(users[0:])
 users.append('Elsa')
@@ -18,5 +17,44 @@ print(users)
 users.extend(['Robert','Aayusha','Jimmy'])
 print(users)
 
-users.insert(0,'Boob')
+users.insert(0,'Bob')
 print(users)
+
+users[0:1]=['Casio','Omega']
+print(users)
+
+users.remove('Casio')
+
+print(users)
+
+users.pop()
+print(users)
+
+del users[0];
+print(users)
+
+# del users;# deletes the whole data
+# print(users)
+
+data.clear()
+print(data)
+
+users[1:2]=['davey']
+
+users.sort()
+print(users)#['Aayusha', 'Dave', 'Elsa', 'Json', 'Robert', 'Sam', 'Superman', 'davey']
+
+users.sort(key=str.lower)
+print(users)#['Aayusha', 'Dave', 'davey', 'Elsa', 'Json', 'Robert', 'Sam', 'Superman']
+
+nums=[1,4,6,3,234,5]
+nums.reverse()
+print(nums)
+
+nums.sort()
+print(nums)
+
+nums.sort(reverse=True)
+print(nums)
+
+
