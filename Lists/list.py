408cb08c589f4d2957a149b79c4f6bb304c98a58
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-04-26 15:09:43 +0545

    list and tuples

--- Lists/list.py
+++ Lists/list.py
@@ -11,50 +11,103 @@ print(users[0:])
 users.append('Elsa')
 print(users)
 
-users+=['Json','Superman']
+users+= ['Jason']
 print(users)
 
-users.extend(['Robert','Aayusha','Jimmy'])
+users.extend(['AyushKoBudi','AyushKoChora'])
 print(users)
 
-users.insert(0,'Bob')
-print(users)
-
-users[0:1]=['Casio','Omega']
-print(users)
-
-users.remove('Casio')
-
-print(users)
-
-users.pop()
-print(users)
-
-del users[0];
-print(users)
-
-# del users;# deletes the whole data
+# users.extend(data)
 # print(users)
 
-data.clear()
+users.insert(0,'BobKochora') #users.insert(index,string)
+print(users)
+
+users[2:2] = ['Eddie','Alex'] 
+print(users)
+
+users[1:3]=['Ronny',"Jenny"]
+print(users)
+
+users.remove('BobKochora')
+print(users)
+
+#pop list
+print(users.pop())
+print(users)
+
+#del list
+del users[0]
+print(users)
+
+# clear data
+data.clear() #clears the data list = emtpty list
 print(data)
 
-users[1:2]=['davey']
-
+##############----------------sorting
+users[1:2]=['dave'] #['Ayush', 'AyushKoBudi', 'Elsa', 'Jason', 'Jenny', 'Sam', 'dave']
 users.sort()
-print(users)#['Aayusha', 'Dave', 'Elsa', 'Json', 'Robert', 'Sam', 'Superman', 'davey']
+print(users)
 
-users.sort(key=str.lower)
-print(users)#['Aayusha', 'Dave', 'davey', 'Elsa', 'Json', 'Robert', 'Sam', 'Superman']
+#sorting with the lowest string
+users.sort(key=str.lower) #['Ayush', 'AyushKoBudi', 'dave', 'Elsa', 'Jason', 'Jenny', 'Sam']
+print(users)
 
-nums=[1,4,6,3,234,5]
-nums.reverse()
+nums=[3,43,23,14,144,45] 
+nums.reverse() #[45, 144, 14, 23, 43, 3]
 print(nums)
 
-nums.sort()
+nums.sort(reverse=True) #[144, 45, 43, 23, 14, 3]
 print(nums)
 
-nums.sort(reverse=True)
+print(sorted(nums,reverse=True))
 print(nums)
+ 
+print("")
+numscopy=nums.copy()
+mynums=list(nums)
+mycopy=nums[:]
+
+mycopy.sort()
+print(mycopy)
+
+print(numscopy)
+print(mynums)
+
+print(type(nums)) #datatype
+print(type(mycopy))
+print(type(mynums))
+
+mylist=list([1,"Neil",True]) #[1, 'Neil', True]
+print(mylist)
+
+
+#tuples = the tuples can be defined as are very mush like list except the data inside the tuples will not change and the order will not change
+mytuple = tuple(('Dave',42,True))
+anotherTuple=(1,2,2,3,4,5)
+print(mytuple)
+print(anotherTuple)
+print(type(anotherTuple))
+print(type(anotherTuple))
+
+#packing tuples
+newList=list(mytuple) #creates a new list out of my tuples
+newList.append('Neil')
+newTuple=tuple(newList)
+print(newTuple)
+
+print("")
+#unpacking tuples
+(one,*two,hey)=anotherTuple
+print(one)
+print(two)
+print(hey)
+
+#to count how mamy occurance are there in the tupes
+print(anotherTuple.count(2))
+
+
+
+
 
 
