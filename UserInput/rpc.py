88daf9cb018709af2ed4b7bb874c1686fe9e4d38
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-04-30 22:58:28 +0545

    recursion and updates in rps

--- UserInput/rpc.py
+++ UserInput/rpc.py
@@ -2,21 +2,20 @@ import random
 import sys
 from enum import Enum
 
-class RPS(Enum):
-    ROCK=1
-    PAPER=2
-    SCISSOR=3
+def play_rps():
+    class RPS(Enum):
+        ROCK=1
+        PAPER=2
+        SCISSOR=3
 
-playagain=True
 
-while playagain:
     print("")
     playerchoice=input("Enter .....\n 1 for Rock \n 2 for Paper \n 3 for Scissors\n\n")
-
+    if(playerchoice not in ["1","2","3"]):
+        print("the choice should be 1,2,3")
+        return play_rps()
     # player choice string ma huncha so you should make it into integer
     player=int(playerchoice)
-    if player<1 or player>3:
-        sys.exit("the choice should be 1,2,3")
 
     computerchoice = random.choice("123")
     computer=int(computerchoice)
@@ -35,13 +34,22 @@ while playagain:
         print("👔 its a tie")
     else:
         print("Python wins the game!!")
-        print("")
+    print("")
 
-    playagain=input("\n\n To play again press Y for yes and N for no")
+    print("Play again?")
+
+    while(True):
+        playagain=input("\n\n To play again press Y for yes and N for no")
+        if playagain.lower() not in ["y","q"]:
+            return play_rps()
+        else:
+            break
     if playagain.lower() =='y':
-        continue
+        return play_rps()
     else:
-        print("Bye!")
-        playagain=False
+        print("BBye")
+        sys.exit("BYE BRO")
 
+
+play_rps()
 
