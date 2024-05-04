ae1dac6f8c3fdeafacd1244df73a9bb6f7435474
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-05-04 11:59:45 +0545

    module

---
+++ module.py
@@ -0,0 +1,9 @@
+import random as rdm #by using as we can make our own alias
+# what is alias (shortform of modules)
+import sys
+from enum import Enum
+# import math
+from math import pi
+
+print(rdm.choice(str(432)))
+print(f"{pi:.2f}")
