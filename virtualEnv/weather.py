009be3b2cbc6ebabc448eaf45d335505af5ec174
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-05-07 12:05:27 +0545

    Weather app

---
+++ virtualEnv/weather.py
@@ -0,0 +1,24 @@
+import requests
+from dotenv import load_dotenv  #helps us to get env variable
+import os
+from pprint import pprint
+
+
+load_dotenv()
+
+def get_current_weather():
+    print("\n *** Get current weather condition ***\n")
+    
+    city=input("please enter a city name: \n")
+
+    request_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
+    # print(request_url)
+    weather_data=requests.get(request_url).json()
+    # pprint(weather_data)
+
+    print(f"Current Weather for {weather_data['name']}")
+    print(f"The Temp is: {weather_data['main']['temp']}")
+    print(f"Feels like : {weather_data['main']['feels_like']} and {weather_data['weather'][0]['description']}.\n")
+
+if __name__ =="__main__":
+    get_current_weather()
