47dbd62879539083ff2363e8f498f7a5c2a934b8
Ayush Shrestha <60820123+ayushstha123@users.noreply.github.com>
2024-05-10 14:42:54 +0545

    template engine

--- django/core/home/views.py
+++ django/core/home/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,"index.html")
+    peoples=[
+        {'name':'Ayush Shrestha','age':23},
+        {'name':'Supriya Shrestha','age':21},
+        {'name':'Abhi lama','age':12},
+        {'name':'Sravan Ghimirey','age':63},
+        {'name':'Sushila suppa','age':34},
+        {'name':'Suman Shrestha','age':13},
+    ]
+
+    vegetables={'pumpkin','ladyfinger','brinjal','carrots'}
+    return render(request,"index.html",context={'peoples':peoples,'vegetables':vegetables})
