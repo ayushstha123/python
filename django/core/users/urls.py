13addec2dc0d1ce189a66f303ca40640903b2508
ayushsth123 <ayyush227@gmail.com>
2024-12-15 22:43:58 +0545

    changes

--- django/core/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/',views.register,name="register"),
-    path('login/',views.user_login,name="user_login"),
-]
-
